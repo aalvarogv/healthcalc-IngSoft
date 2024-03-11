1c98e54fb33d7d4446f7340106a75296e5076702
aalvarogv <alvarogv2003@gmail.com>
2024-03-11 13:30:45 +0100

    Solucion errores clase HealthCalcImpl v2

--- src/main/java/healthcalc/HealthCalcImpl.java
+++ src/main/java/healthcalc/HealthCalcImpl.java
@@ -7,23 +7,23 @@ public class HealthCalcImpl implements HealthCalc {
         
         // Altura introducida incorrecta (menor o igual a 0)
         if (height <= 0) {
-            throw new IllegalArgumentException("La altura introducida debe ser > 0.")
+            throw new IllegalArgumentException("La altura introducida debe ser > 0.");
         }
 
         Character genre = Character.toUpperCase(gender);
         // Género introducido incorrecto
         if (genre != 'M' && genre != 'W') {
-            throw new IllegalArgumentException("El género introducido no es correcto. Debe ser 'M' (hombre) o 'W' (mujer)")
+            throw new IllegalArgumentException("El género introducido no es correcto. Debe ser 'M' (hombre) o 'W' (mujer)");
         
         } else {
             // Altura introducida incorrecta (hombre)
             if (genre == 'M' && height > 250) {
-                throw new IllegalArgumentException("La altura introducida es incorrecta para un hombre.")
+                throw new IllegalArgumentException("La altura introducida es incorrecta para un hombre.");
             }
 
             // Altura introducida incorrecta (mujer)
             if (genre == 'W' && height > 230) {
-                throw new IllegalArgumentException("La altura introducida es incorrecta para una mujer.")
+                throw new IllegalArgumentException("La altura introducida es incorrecta para una mujer.");
             }
         }
 
@@ -33,7 +33,7 @@ public class HealthCalcImpl implements HealthCalc {
 
             // Peso ideal erróneo (hombre)
             if (IW < 0) {
-                throw new IllegalArgumentException("El peso ideal calculado (hombre) es erróneo (menor que 0).")
+                throw new IllegalArgumentException("El peso ideal calculado (hombre) es erróneo (menor que 0).");
             } else {return IW;}
 
         } else {
@@ -41,7 +41,7 @@ public class HealthCalcImpl implements HealthCalc {
 
             // Peso ideal erróneo (mujer)
             if (IW < 0) {
-                throw new IllegalArgumentException("El peso ideal calculado (mujer) es erróneo (menor que 0).")
+                throw new IllegalArgumentException("El peso ideal calculado (mujer) es erróneo (menor que 0).");
             } else {return IW;}
         }
     }
@@ -51,33 +51,33 @@ public class HealthCalcImpl implements HealthCalc {
 
         // Altura introducida incorrecta (menor o igual a 0)
         if (height <= 0) {
-            throw new IllegalArgumentException("La altura introducida debe ser > 0.")
+            throw new IllegalArgumentException("La altura introducida debe ser > 0.");
         }
 
         // Peso introducido incorrecto (menor o igual a 0)
         if (weight <= 0) {
-            throw new IllegalArgumentException("El peso introducido debe ser > 0.")
+            throw new IllegalArgumentException("El peso introducido debe ser > 0.");
         }
 
         // Edad introducida incorrecta (negativa o mayor que 140)
         if (age <= 0) {
-            throw new IllegalArgumentException("La edad introducida debe estar entre 0 y 140.")
+            throw new IllegalArgumentException("La edad introducida debe estar entre 0 y 140.");
         }
 
         Character genre = Character.toUpperCase(gender);
         // Género introducido incorrecto
         if (genre != 'M' && genre != 'W') {
-            throw new IllegalArgumentException("El género introducido no es correcto. Debe ser 'M' (hombre) o 'W' (mujer)")
+            throw new IllegalArgumentException("El género introducido no es correcto. Debe ser 'M' (hombre) o 'W' (mujer)");
         
         } else {
             // Altura introducida incorrecta (hombre)
             if (genre == 'M' && height > 250) {
-                throw new IllegalArgumentException("La altura introducida es incorrecta para un hombre.")
+                throw new IllegalArgumentException("La altura introducida es incorrecta para un hombre.");
             }
 
             // Altura introducida incorrecta (mujer)
             if (genre == 'W' && height > 230) {
-                throw new IllegalArgumentException("La altura introducida es incorrecta para una mujer.")
+                throw new IllegalArgumentException("La altura introducida es incorrecta para una mujer.");
             }
         }
 
@@ -87,7 +87,7 @@ public class HealthCalcImpl implements HealthCalc {
 
             // Tasa Metabólica Basal errónea (hombre)
             if (BMR < 0) {
-                throw new IllegalArgumentException("La Tasa Metabólica Basal calculada (hombre) es errónea (menor que 0).")
+                throw new IllegalArgumentException("La Tasa Metabólica Basal calculada (hombre) es errónea (menor que 0).");
             } else {return BMR;}
 
         } else {
@@ -95,7 +95,7 @@ public class HealthCalcImpl implements HealthCalc {
 
             // Tasa Metabólica Basal errónea (mujer)
             if (BMR < 0) {
-                throw new IllegalArgumentException("La Tasa Metabólica Basal calculada (mujer) es errónea (menor que 0).")
+                throw new IllegalArgumentException("La Tasa Metabólica Basal calculada (mujer) es errónea (menor que 0).");
             } else {return BMR;}
         }
     }
